b8a3e3be3c0dd49a1d41df73ecc618ddbcad41a9
Plikt <desotaelianna@gmail.com>
2021-07-21 08:07:14 -0400

    Update 20210627_gearpermutations_finalcode.py

--- 20210627_gearpermutations_finalcode.py
+++ 20210627_gearpermutations_finalcode.py
@@ -21,7 +21,7 @@ permwb = wb.create_sheet('gearpermutations', 0)
 #Gear names - can be changed to whatever you'd like to label the gears
 column = ['compound type','Speed Ratio','Gear 1', 'Gear 2', 'Gear 3', 'Gear 4', 'Gear 5', 'Gear 6', 'Gear 7'] 
 
-values = [2, 4, 6, 8, 10, 16, 24] #array of gears w desired teeth numbers
+values = [4, 6, 8, 12, 16, 24,36] #array of gears w desired teeth numbers
 
 per = [] #storage of the number of permultations given len(values) choose i
 sol = [] #solution array
@@ -29,11 +29,14 @@ gearspeed = 1
 index = 0
 
 def genbin(n, bs=''):
+    
     if len(bs) == n:
+        bs = "x" + bs#
         binom.append(bs)
     else:
         genbin(n, bs + '0')
         genbin(n, bs + '1')
+    
 
 #For loop which stores all the permutations in which you can choose i of the length of the values array
 for choose in range(1, len(values)+1):
@@ -51,6 +54,7 @@ for choose in range(1, len(values)+1):
         #iterates over the length of the permutations given len(values) choose (choose) from above
         for rep in range(len(binom)):               
             sol.append(list(per[currentperm])) #appends per at a given index to the solution array
+            #binom[rep] = "x" + binom[rep]
             sol[-1].insert(0,binom[rep]) #inserts the replication number to track which combo is happening - not elegant 
             gearspeed = 1
    
@@ -58,7 +62,7 @@ for choose in range(1, len(values)+1):
             #checks if the replication is 0, because the calculation for no compunds is different than with compounds
             for index in range(len(per[currentperm])-1):
                 #print(binom)
-                if binom[rep][index] == '0': 
+                if binom[rep][index+1] == '0': 
                     gearspeed = gearspeed*(per[currentperm][index]/per[currentperm][index+1])
                                 
             sol[-1].insert(1, gearspeed)
@@ -70,5 +74,5 @@ df = pd.DataFrame(sol, columns = column)
 sort = df.sort_values(by = ['Speed Ratio'])
 print(sort)
 
-sort.to_csv("C:/Users/Psych account/Documents/gearpermutations/20210107gearpermutations.csv")                  
+sort.to_csv("C:/Users/Psych account/Documents/gearpermutations/20210720gearpermutations.csv")                  
 
